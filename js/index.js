5aa3b1b8b86a437174e69e0328571d280a9037f9
Vinh <95878001+VinhHinh@users.noreply.github.com>
2022-11-19 20:04:48 +0700

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,13 @@
+let popup = document.getElementById("popup");
+let audio = document.getElementById("audio");
+function openPopup() {
+    popup.classList.add("open-popup")
+}
+function closePopup() {
+    popup.classList.remove("open-popup")
+}
+function playaudio(){
+   var a = new Audio("video-1659118097.mp3");
+   a.play();
+   a.loop=true;
+}

d48a3f2817e2129487fd46f4606175f46e4029b6
Vinh <95878001+VinhHinh@users.noreply.github.com>
2022-12-18 01:31:36 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -13,6 +13,6 @@ function playaudio(){
    a.play();
 }
 function Hide(){
-    var htmsl = '<iframe id="loichuc1" style="position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;" src="/html/loichuc1.html" ></iframe>'
+    var htmsl = '<iframe id="loichuc1" style="position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;" src="html/loichuc1.html" ></iframe>'
     popup.insertAdjacentHTML('afterend',htmsl)
 }

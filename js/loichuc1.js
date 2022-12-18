ddb19c41b8bdbdbc147f2dd4040cbdaef5e60072
QuocVinh <quocvinhh99f9@gmail.com>
2022-12-18 23:20:20 +0700

    update

--- js/loichuc1.js
+++ js/loichuc1.js
@@ -3,6 +3,6 @@ let back = parent.document.getElementById("back");
 let loichuc = parent.document.getElementById("loichuc1");
 function Show(){
     loichuc.remove()
-    var htmsl = '<iframe id="loichuc2" style="position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;" src="../html/loichuc2.html" ></iframe>'
+    var htmsl = '<iframe id="loichuc2" style="position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;" src="./html/loichuc2.html" ></iframe>'
     back.insertAdjacentHTML('afterend',htmsl)
 }
--- js/loichuc2.js
+++ js/loichuc2.js
@@ -3,6 +3,6 @@ let back = parent.document.getElementById("back");
 let loichuc = parent.document.getElementById("loichuc2");
 function Show2(){
     loichuc.remove()
-    var htmsl = '<iframe id="end" style="position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;" src="../html/end.html" ></iframe>'
+    var htmsl = '<iframe id="end" style="position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;" src="./html/end.html" ></iframe>'
     back.insertAdjacentHTML('afterend',htmsl)
 }
